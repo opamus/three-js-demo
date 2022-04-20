1cf74ab397bee006a53eec61754626cc2fd69327
Olli Lehmusvuori <ollilehmusvuori@gmail.com>
2022-04-20 16:57:41 +0300

    Make a small planet with texture

--- main.js
+++ main.js
@@ -10,21 +10,50 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const renderer = new THREE.WebGLRenderer();
+// Textures
+const textureLoader = new THREE.TextureLoader();
+const normalTexture = textureLoader.load('/textures/normalmap.png');
+
+// Canvas
+const canvas = document.querySelector('#bg');
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Objects
+const geometry = new THREE.SphereGeometry();
 
-camera.position.z = 5;
+// Materials
+const material = new THREE.MeshStandardMaterial({
+  color: 0x55ada8,
+});
+material.normalMap = normalTexture;
+
+// Mesh
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+// Lighting
+// const light = new THREE.AmbientLight(0x008000); // soft white light
+// scene.add(light);
+
+const pointLight = new THREE.PointLight(0x55ada8, 0.5);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 4;
+scene.add(camera);
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  sphere.rotation.y += 0.005;
   renderer.render(scene, camera);
 }
 animate();
